1de1a1b38baee8eff2f94d5548c732708b8fe2ac
Sanjay Chaudhari <sanjay_chaudhari@hotmail.com>
2020-08-25 10:19:14 +0530

    Update WA.py
    Removed secrets

--- WA.py
+++ WA.py
@@ -3,14 +3,14 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACdb753b826870e21ef72327cbd4621f2b'
-auth_token = '3a450bea33b063e33d5916837fcbcfa9'
+account_sid = 'accountSid'
+auth_token = 'authToken'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body='Hello there - Sending msg using PYTHON & TWILIO - NEW !',
                               from_='whatsapp:+14155238886',
-                              to='whatsapp:+919766649483'
+                              to='whatsapp:+919966649483'
                           )
 
 print(message.sid)
